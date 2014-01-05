25663c2cf2cc781d845e9ee0a39c3b0fbaf331b6
Val Tenyotkin <val@tenyotk.in>
2014-01-05 15:58:32 -0800

    Lots of stuff.  First, fully-functional version.

--- chash.h
+++ chash.h
@@ -6,9 +6,8 @@
 #define CHASH_FALSE     0
 #define CHASH_INTEGER   long
 #define CHASH_CHASH     unsigned long long
-#define CHASH_EMPTY     0
-#define CHASH_SET       1
-
+#define CHASH_EMPTY     (struct cHashlet *)0
+#define CHASH_SET       (struct cHashlet *)1
 
 #define KEY_LEN         32
 #define OCTET           8
@@ -95,16 +94,74 @@ void cHash_set(cHash *hash, char *key, long value) {
     // compute the index of the *key
     int index = cHash_hash(key, strlen(key)) % hash->size;
     
-    // if index is unused
-    if(hash->hashlets[index].next == CHASH_EMPTY) {
+    // acquire the head hashlet
+    cHashlet *link = &hash->hashlets[index];
+
+    // if index is unused, initialize it
+    if(link->next == CHASH_EMPTY) {
+        #ifdef ebug
+            printf("Unused index %d.  %s = %ld\n", index, key, value);
+        #endif
         // store the key
-        strcpy(hash->hashlets[index].key, key);
+        strcpy(link->key, key);
         // store the value
-        hash->hashlets[index].value = value;
+        link->value = value;
         // set the .next to indicate the end of the hashlet chain
-        hash->hashlets[index].next = (struct cHashlet *)CHASH_SET;
+        link->next = CHASH_SET;
+        // done
+        return;
     }
 
+    #ifdef ebug
+        printf("Hash collision. %s vs %s\n", link->key, key);
+    #endif
+    // if the index is used, loop through the chain
+    while(1) {
+        #ifdef ebug
+            printf("... %s\n", link->key);
+        #endif
+
+        // if the key exactly matches an existing key, overwrite the value
+        if(!strcmp(link->key, key)) {
+            #ifdef ebug
+                printf("Overwrite.  %s = %ld\n", key, value);
+            #endif
+            link->value = value;
+            return;
+        }
+
+        // stop when the end of the chain has been reached
+        if(link->next == CHASH_SET)
+            break;
+
+        // proceed to the next link
+        link = (cHashlet *)link->next;
+
+    }
+
+    // new key-value pair
+    #ifdef ebug
+        printf("New entry in chain %d. %s = %ld\n", index, key, value);
+    #endif
+
+    // allocate the ram
+    // DO TO: get the ram from the incrementally-reallocated pool
+    if(!(link->next = (struct cHashlet *) malloc(sizeof(cHashlet)))) {
+        #ifdef ebug
+            printf("Unable to allocate RAM for the new hashlet.");
+        #endif
+        return;
+    }
+
+    // point the link to the newly-created hashlet
+    link = (cHashlet *)link->next;
+
+    // store the key
+    strcpy(link->key, key);
+    // store the value
+    link->value = value;
+    // bookkeeping
+    link->next = CHASH_SET;
 }
 
 // get
@@ -113,11 +170,43 @@ long cHash_get(cHash *hash, char *key) {
     // compute the index of the *key
     int index = cHash_hash(key, strlen(key)) % hash->size;
 
-    // if the hashlet is set and at the end of the chain
-    if(hash->hashlets[index].next == (struct cHashlet *)CHASH_SET)
-        // return the value
-        return hash->hashlets[index].value;
+    // acquire the head hashlet
+    cHashlet *link = &hash->hashlets[index];
 
+    // if the hash is undefined, return false
+    if(link->next == CHASH_EMPTY) {
+        #ifdef ebug
+            printf("Key %s does not exist.\n", key);
+        #endif
+        return CHASH_FALSE;
+    }
+
+    #ifdef ebug
+        printf("Searching for %s in chain %d.\n", key, index);
+    #endif
+    // otherwise travel down the chain
+    while(1) {
+        #ifdef ebug
+            printf("... currently on %s\n", link->key);
+        #endif
+        // if the key matches, return the value
+        if(!strcmp(link->key, key))
+            return link->value;
+
+        // stop when the end of the chain has been reached
+        if(link->next == CHASH_SET) {
+            #ifdef ebug
+                printf("... chain ends with %s\n", link->key);
+            #endif
+            break;
+        }
+
+        // proceed to the next link
+        link = (cHashlet *)link->next;
+
+    }
+
+    // return false if the key doens't exist
     return CHASH_FALSE;
 }
 
